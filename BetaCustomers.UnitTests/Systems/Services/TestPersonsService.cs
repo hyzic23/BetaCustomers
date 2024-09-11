5a3d956756ec723892eba4e42f0dfc70f7c04fec
hyzic <isaac.oghenevwapo@gmail.com>
2024-09-11 11:50:16 -0400

    Added Unit test to GetCustomerById

--- BetaCustomers.UnitTests/Systems/Services/TestPersonsService.cs
+++ BetaCustomers.UnitTests/Systems/Services/TestPersonsService.cs
@@ -1,3 +1,4 @@
+using BetaCustomers.API.IServices;
 using BetaCustomers.API.Models;
 using BetaCustomers.API.Services;
 using BetaCustomers.UnitTests.Fixtures;
@@ -8,7 +9,15 @@ namespace BetaCustomers.UnitTests.Systems.Services;
 
 public class TestPersonsService
 {
-   
+   private readonly PersonsService _sut;
+   private readonly Mock<IPersonsService> _personServiceMock = new Mock<IPersonsService>();
+
+   public TestPersonsService()
+   {
+      _sut = new PersonsService();
+      //_sut = new PersonsService(_personServiceMock.Object);
+   }
+
    [Fact]
    public void LoadPerson_ValidCall()
    {
@@ -24,5 +33,48 @@ public class TestPersonsService
       //result.Should().BeOfType<List<Person>>();
       //Assert.True(result != null);
       Assert.IsType<List<Person>>(result);
+      Assert.Equal(expectedResponse._persons.Count, result.Count);
+   }
+   
+   [Fact]
+   public void LoadPerson_With_EmptyList()
+   {
+      //Arrange
+      var expectedResponse = new PersonsFixture();
+      var sut = new PersonsService();
+      var mockHandler = new Mock<PersonsService>();
+
+      //Act
+      var result = sut.LoadAll();
+
+      //Assert
+      //result.Should().BeOfType<List<Person>>();
+      Assert.IsType<List<Person>>(new List<Person>());
+   }
+   
+   [Fact]
+   public void GetPersonById_ShouldReturnPerson_WhenIdExists()
+   {
+      //Arrange
+      var personId = 1;
+      var firstName = "John";
+      var lastName = "Doe";
+
+      var personDto = new Person
+      {
+         Id = personId,
+         FirstName = firstName,
+         LastName = lastName
+      };
+
+      _personServiceMock
+         .Setup(x => x.GetPersonById(personId))
+         .Returns(personDto);
+
+      //Act
+      var person = _sut.GetPersonById(personId);
+
+      //Assert
+      Assert.Equal(personId, person.Id);
    }
 }
