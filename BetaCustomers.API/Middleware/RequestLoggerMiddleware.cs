40f911d55e650a6f8cfa708c310d0425fc08030a
hyzic <isaac.oghenevwapo@gmail.com>
2024-09-19 10:14:18 -0400

    Add Logger to RequestLoggerMiddleware implementation

--- BetaCustomers.API/Middleware/RequestLoggerMiddleware.cs
+++ BetaCustomers.API/Middleware/RequestLoggerMiddleware.cs
@@ -3,10 +3,13 @@ namespace BetaCustomers.API.Middleware;
 public class RequestLoggerMiddleware
 {
     private readonly RequestDelegate _next;
+    private readonly ILogger<RequestLoggerMiddleware> _logger;
 
-    public RequestLoggerMiddleware(RequestDelegate next)
+    public RequestLoggerMiddleware(RequestDelegate next, 
+                                   ILogger<RequestLoggerMiddleware> logger)
     {
         _next = next;
+        _logger = logger;
     }
 
     public async Task Invoke(HttpContext context)
@@ -14,7 +17,7 @@ public class RequestLoggerMiddleware
         // Log information about the incoming request
         var requestPath = context.Request.Path;
         var requestMethod = context.Request.Method;
-        Console.WriteLine($"Incoming request: {requestMethod} {requestPath}");
+        _logger.LogInformation($"Incoming request: {requestMethod} {requestPath}");
         
         // Call the next Middleware in the pipeline
         await _next(context);
